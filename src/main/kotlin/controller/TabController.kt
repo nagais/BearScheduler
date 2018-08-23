e5818f8be2d01cb1540085aeb51f8f116f53025d
nagais <nagais@istyle.co.jp>
2018-08-23 14:15:45 +0900

    レイアウトにNodeの追加とController側に最低限の処理を追加

--- src/main/kotlin/controller/TabController.kt
+++ src/main/kotlin/controller/TabController.kt
@@ -1,7 +1,25 @@
 package controller
 
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.fxml.FXML
+import javafx.scene.Node
+import javafx.scene.control.ListView
+import javafx.scene.control.TextField
+import javafx.scene.input.KeyEvent
+
 class TabController {
 
+    @FXML private lateinit var textField: TextField
+    @FXML private lateinit var listView: ListView<Node>
+
+    private val listItem: ObservableList<Node> = FXCollections.observableArrayList()
+
     fun initialize() {
+        listView.items = listItem
+    }
+
+    @FXML fun onKeyPress(key: KeyEvent) {
+
     }
 }

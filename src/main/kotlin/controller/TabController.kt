1d2e408e23096d0f977f613d870d1e1f4e3362a8
nagais <nagais@istyle.co.jp>
2018-08-23 23:13:34 +0900

    feat : enter押下時の処理を追加

--- src/main/kotlin/controller/TabController.kt
+++ src/main/kotlin/controller/TabController.kt
@@ -5,12 +5,15 @@ import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.scene.control.ListView
 import javafx.scene.control.TextField
+import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
 
 class TabController {
 
-    @FXML private lateinit var textField: TextField
-    @FXML private lateinit var listView: ListView<String>
+    @FXML
+    private lateinit var textField: TextField
+    @FXML
+    private lateinit var listView: ListView<String>
 
     private val listItem: ObservableList<String> = FXCollections.observableArrayList()
 
@@ -18,7 +21,10 @@ class TabController {
         listView.items = listItem
     }
 
-    @FXML fun onKeyPress(key: KeyEvent) {
-
+    @FXML
+    fun onKeyPress(key: KeyEvent) {
+        if (key.code == KeyCode.ENTER && textField.text.isNotEmpty()) {
+            listItem.add(textField.text)
+        }
     }
 }

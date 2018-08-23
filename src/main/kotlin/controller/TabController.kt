dcc8a17bebf1d442aab4291f0be3c77b0145aa40
nagais <nagais@istyle.co.jp>
2018-08-23 23:12:16 +0900

    fix : listの型を修正

--- src/main/kotlin/controller/TabController.kt
+++ src/main/kotlin/controller/TabController.kt
@@ -3,7 +3,6 @@ package controller
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
 import javafx.fxml.FXML
-import javafx.scene.Node
 import javafx.scene.control.ListView
 import javafx.scene.control.TextField
 import javafx.scene.input.KeyEvent
@@ -11,9 +10,9 @@ import javafx.scene.input.KeyEvent
 class TabController {
 
     @FXML private lateinit var textField: TextField
-    @FXML private lateinit var listView: ListView<Node>
+    @FXML private lateinit var listView: ListView<String>
 
-    private val listItem: ObservableList<Node> = FXCollections.observableArrayList()
+    private val listItem: ObservableList<String> = FXCollections.observableArrayList()
 
     fun initialize() {
         listView.items = listItem

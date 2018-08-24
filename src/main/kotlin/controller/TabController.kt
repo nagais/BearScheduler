abe919b7a2184582ad0d036972518deb32c449d8
nagais <nagais@istyle.co.jp>
2018-08-24 10:00:33 +0900

    feat : セルのカスタム実装の大枠を作成

--- src/main/kotlin/controller/TabController.kt
+++ src/main/kotlin/controller/TabController.kt
@@ -1,25 +1,35 @@
 package controller
 
+import common.CustomTaskCell
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
 import javafx.fxml.FXML
-import javafx.scene.Node
+import javafx.scene.control.ListCell
 import javafx.scene.control.ListView
 import javafx.scene.control.TextField
+import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
+import javafx.util.Callback
 
 class TabController {
 
-    @FXML private lateinit var textField: TextField
-    @FXML private lateinit var listView: ListView<Node>
+    @FXML
+    private lateinit var textField: TextField
+    @FXML
+    private lateinit var listView: ListView<String>
 
-    private val listItem: ObservableList<Node> = FXCollections.observableArrayList()
+    private val listItem: ObservableList<String> = FXCollections.observableArrayList()
 
     fun initialize() {
         listView.items = listItem
+        listView.cellFactory = Callback<ListView<String>, ListCell<String>> { CustomTaskCell() }
     }
 
-    @FXML fun onKeyPress(key: KeyEvent) {
-
+    @FXML
+    fun onKeyPress(key: KeyEvent) {
+        if (key.code == KeyCode.ENTER && textField.text.isNotEmpty()) {
+            listItem.add(textField.text)
+            textField.clear()
+        }
     }
 }

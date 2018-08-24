f1e2ec21970a25ab86c252e9e575316c29d1f4d2
nagais <nagais@istyle.co.jp>
2018-08-24 20:50:03 +0900

    feat : カスタムセルを利用したタスクの追加と一部layoutの修正

--- src/main/kotlin/common/CustomTaskCell.kt
+++ src/main/kotlin/common/CustomTaskCell.kt
@@ -2,26 +2,35 @@ package common
 
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
-import javafx.scene.Parent
+import javafx.scene.Node
 import javafx.scene.control.Label
 import javafx.scene.control.ListCell
+import javafx.scene.layout.AnchorPane
 import util.AppResource
 
 class CustomTaskCell : ListCell<String>() {
     @FXML
     private lateinit var task: Label
+    @FXML
+    private lateinit var taskPane: AnchorPane
 
-    private val loader:FXMLLoader = AppResource.getLayoutLoader(AppResource.Layout.TASK_CELL)
+    private val loader: FXMLLoader = AppResource.getLayoutLoader(AppResource.Layout.TASK_CELL)
 
     init {
         loader.setController(this)
-        loader.load<Parent>()
+        loader.load<Node>()
     }
 
     override fun updateItem(item: String?, empty: Boolean) {
         super.updateItem(item, empty)
-        item?.let {
-            this.task.text = it
+        if (empty) {
+            text = null
+            graphic = null
+        } else {
+            item?.let {
+                this.task.text = it
+            }
+            graphic = taskPane
         }
     }
 }
